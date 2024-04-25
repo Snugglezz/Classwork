15332e2aaee4e50100952aebf15b46bf62f6263a
isaiah <ihensley@bostonk12.org>
2024-04-25 10:35:11 -0400

    integer_input.py

---
+++ Week 16/practice3.py
@@ -0,0 +1,21 @@
+#Isaiah Hensley
+#20204/25/40
+
+#this is how you create an empty list
+a=[]
+
+#this is how you create a list with values
+b=[1,2,3]
+
+print(a)
+print(b)
+
+a+=["isaiah"]
+
+print(a)
+print(b)
+
+b+=[4]
+
+print(a)
+print(b)
