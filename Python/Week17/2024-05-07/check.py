18222158fd7e34a84cd442c36d025b7e5ea9ece4
isaiah <ihensley@bostonk12.org>
2024-05-07 10:21:19 -0400

    integer_input.py

---
+++ Python/Week17/2024-05-07/check.py
@@ -0,0 +1,30 @@
+# Isaiah Hensley
+# 2024-05-07
+
+# creating an empty list
+a = []
+
+# creating an operator to add to the list
+a += [1]
+a += [2]
+a += [3]
+
+# printing the list
+print(a)
+
+# creating an empty list named b
+b = []
+
+# using the function to add to the list b
+b.append(1)
+b.append(2)
+b.append(3)
+
+# printing the list
+print(b)
+
+# postional comparison
+print(a==b)
+
+# object comparison
+print(a is b)
