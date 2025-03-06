f476994984595051cc0ab872a497418d4b2b8178
Snugglezz <ihensley@bostonk12.org>
2025-03-06 14:06:56 -0500

    commit to github

---
+++ 2023-2024/Python/2024-2025/week_12/2025-03-03/BFB.py
@@ -0,0 +1,9 @@
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+brb = input("Who is the best blow for blow fighter?")
+print(f"{brb} is the best blow for blow fighter")
---
+++ 2023-2024/Python/2024-2025/week_12/2025-03-03/plant2.py
@@ -0,0 +1,8 @@
+def main()
+plant = int(input("Pick a number: "))
+plant2 = int(input("Pick another number: "))
+print(f"{plant} x {plant2} is {plant * plant2}.")
+
+
+if __name__ == '__main__':
+    main()
