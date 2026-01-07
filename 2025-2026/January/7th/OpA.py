31d721edf1537f8a592edc04ef0ddbb340087a79
447240 <ihensley@bostonk12.org>
2026-01-07 10:46:19 -0500

    Operations Assisgnment

---
+++ 2025-2026/January/7th/OpA.py
@@ -0,0 +1,24 @@
+def main():
+    pass
+my_list = []
+print(my_list)
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+fruits.append("grape")
+fruits += ["mango"]
+print(fruits)
+fruits.insert(2, "kiwi")
+print(fruits)
+
+if __name__ == '__main__':
+
+
+
+
+
+
+
+
+
+
+
