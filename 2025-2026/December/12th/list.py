ae54a8f45cc325b4e799fdbe14983946e74415c2
447240 <ihensley@bostonk12.org>
2026-01-07 11:29:05 -0500

    Operations Assisgnment

---
+++ 2025-2026/December/12th/list.py
@@ -0,0 +1,12 @@
+#Isaiah Hensley
+#12-12-25
+def main():
+    #how to create an empty list5
+    a = []
+    b = list()
+    #how to create a list with items
+    c = [1 , 2]
+    #how to find the amount of items in a list
+    print(len(a))
+if __name__ == '__main__':
+    main() `AQ  W23e`                           
