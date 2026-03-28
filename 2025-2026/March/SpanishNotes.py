fe97e1709f489c4041cc0a91ed5bfcdc4b110268
Snugglezz <ihensley@bostonk12.org>
2026-03-28 17:17:28 -0400

    spanish program

--- 2025-2026/March/SpanishNotes.py
+++ 2025-2026/March/SpanishNotes.py
@@ -1,13 +1,20 @@
+import random
 def main():
+    trans = {}
     with(open("Span.txt","r")) as notes:
         for line in notes:
-            print(line)
-
-
-    # file = open("Span.txt", "r")
-    # guess = file.readline()
-    # while guess != "":
-    #    answer = input(f"what is the correct translation for{guess}")
+            span = line.strip()
+            store = span.split(":")
+            key = store[1]
+            val = store[0]
+            trans[key] = val
+    # print(trans)
+    item = random.choice(list(trans.keys()))
+    answer = input(f"Whats the translation to {item}? ")
+    if answer == trans.get(item) :
+        print(f"Correct! the Answer is {trans[item]}")
+    else:
+        print(f"Wrong! the Answer is {trans[item]}")
 
 
 if __name__ == '__main__':
