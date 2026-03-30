5d33cbdede8eb6722a770f9ddd70809fddf2e426
Snugglezz <ihensley@bostonk12.org>
2026-03-30 07:58:25 -0400

    spanish program

--- 2025-2026/March/SpanishNotes.py
+++ 2025-2026/March/SpanishNotes.py
@@ -9,13 +9,15 @@ def main():
             val = store[0]
             trans[key] = val
     # print(trans)
-    item = random.choice(list(trans.keys()))
-    answer = input(f"Whats the translation to {item}? ")
-    if answer == trans.get(item) :
-        print(f"Correct! the Answer is {trans[item]}")
-    else:
-        print(f"Wrong! the Answer is {trans[item]}")
-
+    quit = "Y"
+    while quit == "Y":
+        item = random.choice(list(trans.keys()))
+        answer = input(f"Whats the translation to {item}? ")
+        if answer == trans.get(item) :
+            print(f"Correct! the Answer is {trans[item]}")
+        else:
+            print(f"Wrong! the Answer is {trans[item]}")
+        quit = input("Do you want to continue? Y/N ")
 
 if __name__ == '__main__':
     main()
